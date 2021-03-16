9464daa63eda3a0c2ee9be87f3391e5457328edc
ji sun lee <ellijs@hotmail.com>
2021-03-16 05:42:13 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,8 +1,3 @@
-require_relative './current_age_for_birth_year.rb'
-
-puts "What year were you born?"
-birth_year = gets.to_i
-
-use_age = current_age_for_birth_year(birth_year)
-
-puts "You are: " + users_age.tp_s + "years old."
+def current_age_for_birth_year(birth_year)
+  Time.now.year - birth_year
+end
